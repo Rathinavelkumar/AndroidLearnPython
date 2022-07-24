5cb6097d27c9b03f18ae0ea5377a77fc7c9ddfce
Rathinavelkumar <Rathinavelkumar.Murugan@gmail.com>
2022-07-24 17:46:26 +0530

    version 3.0

--- app/src/main/java/com/rethna/android/learnpython/WebViewClientImpl.java
+++ app/src/main/java/com/rethna/android/learnpython/WebViewClientImpl.java
@@ -15,7 +15,7 @@ public class WebViewClientImpl extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView webView, String url){
-        if(url.indexOf("github.io") > -1) return false;
+        if(url.indexOf("rathnaschools.com") > -1) return false;
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         activity.startActivity(intent);
         return true;
